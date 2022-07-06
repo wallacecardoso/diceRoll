28661d57c0b448350c4cf0cfcc59b16e767cd512
wallace cardoso <wallacedossantoscardoso@live.com>
2022-07-06 01:12:27 -0300

    add dice images and updated the logic.

--- app/src/main/java/com/cardoso/diceroller/MainActivity.kt
+++ app/src/main/java/com/cardoso/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cardoso.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /**
+         * forma de selecionar o botão para rolar o dado.
+         * **/
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             val toast = getString(R.string.cd_toast_main)
@@ -19,16 +23,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Função para rolar o dado e adicionar seu resultado no textView do app.
+     * **/
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-    }
-}
-    class Dice (private val numSides: Int) {
-        fun roll(): Int {
-            return (1..numSides).random()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
         }
     }
+}
+
+/**
+ * Classe que foi instanciada para produção do objeto dado(utilizado pela activity main do meu app).
+ * **/
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
 
